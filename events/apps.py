2cba717d78b1bb2e7a64c25fbb0153aef193a514
Beyzaeo <s240307031@ankarabilim.edu.tr>
2025-12-06 16:58:39 +0300

    Remove event_detail route from apps.py
    Removed the event_detail route and its associated function.

--- events/apps.py
+++ events/apps.py
@@ -4,8 +4,4 @@ from django.apps import AppConfig
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
-    
-@app.route("/event/<int:event_id>")
-def event_detail(event_id):
-    event = Event.query.get_or_404(event_id)
-    return render_template("event_detail.html", event=event)
+ 
